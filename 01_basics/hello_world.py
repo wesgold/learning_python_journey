285d492068db1f24292c21ef81f0ce9633db1920
Wesley Goldstein <wesdgold@gmail.com>
2025-08-30 00:41:03 -0400

    Learned variables and basic data types

--- 01_basics/hello_world.py
+++ 01_basics/hello_world.py
@@ -2,3 +2,9 @@ print("Hello World")
 print("My name is Wesley")
 print("I am 20 years old")
 print("I'm learning Python to become a cracked AI engineer")
+
+# Add interactive input
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+age_as_number = int(age)
+print(f"Hello {name}, you are {age_as_number} years old!")
---
+++ 01_basics/variables.py
@@ -0,0 +1,13 @@
+#strings
+name = "Wesley"
+goal = "AI Engineer"
+#numbers
+age = 20
+year = 2025
+height = 6.1
+#Booleans
+is_learning = True
+
+print(f"My name is {name}")
+print(f"In {year}, I'm learning to become an {goal}")
+print(f"Am I learning? {is_learning}")
