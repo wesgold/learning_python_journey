dcca67671df94a7d0c57116ec97dace1b7eea10b
Wesley Goldstein <wesdgold@gmail.com>
2025-08-31 14:49:28 -0400

    Learned while and for loops

---
+++ 01_basics/loops.py
@@ -0,0 +1,15 @@
+# While lopp
+count = 1
+while count <= 5:
+    print(f"Count is : {count}")
+    count += 1
+
+# For loop
+print("\nFor loop with range:")
+for numbers in range(1, 6):
+    print(f"Number: {numbers}")
+
+# Looping through a list
+languages = ["Python", "JavaScript", "SQL"]
+for lang in languages:
+    print(f"I want to learn {lang}")
